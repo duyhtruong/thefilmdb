2268d357ddd8e8136495f6bcbcd0266c76cc00ac
Duy Truong <duytruongh@gmail.com>
2019-03-07 08:36:27 -0800

    Updates

---
+++ precache-manifest.b003076d585e5af2e1fdd58160a99537.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c3a8088e9492282511f0c838ae8a2dd",
+    "url": "/thefilmdb/static/media/filmdblogo2.0c3a8088.png"
+  },
+  {
+    "revision": "56601dd9419052cbd475",
+    "url": "/thefilmdb/static/js/runtime~main.56601dd9.js"
+  },
+  {
+    "revision": "e0231ab7815e25df6211",
+    "url": "/thefilmdb/static/js/main.e0231ab7.chunk.js"
+  },
+  {
+    "revision": "3d90eec51e28d372ccf4",
+    "url": "/thefilmdb/static/js/1.3d90eec5.chunk.js"
+  },
+  {
+    "revision": "e0231ab7815e25df6211",
+    "url": "/thefilmdb/static/css/main.477e55ba.chunk.css"
+  },
+  {
+    "revision": "c4ed13d6c5ba076d659654699e976315",
+    "url": "/thefilmdb/index.html"
+  }
+];
